303994d8e650d51d5bca76303eea4cc15e452968
vitalis-virtus <vshayuk@gmail.com>
2022-12-20 18:38:57 +0200

    add routes and main.go

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/gin-gonic/gin"
+	"github.com/vitalis-virtus/ecommerce-go/controllers"
+	"github.com/vitalis-virtus/ecommerce-go/db"
+	"github.com/vitalis-virtus/ecommerce-go/middleware"
+	"github.com/vitalis-virtus/ecommerce-go/routes"
+)
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp:   true,
+		TimestampFormat: "2006-01-02 15:04:05",
+		ForceColors:     true,
+		DisableColors:   false,
+	})
+
+	log.SetLevel(log.DebugLevel)
+}
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	app := controllers.NewApplication(db.ProductData(db.Client, "Products"), db.UserData(db.Client, "Users"))
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/removeitem", app, RemoveItem())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
+
+	log.Fatal(router.Run(":" + port))
+}
