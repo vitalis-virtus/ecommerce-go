fa6464ab357e02e572ba71ca3b857c402fb78054
vitalis-virtus <vshayuk@gmail.com>
2022-12-21 19:38:58 +0200

    initialized all controllers functions

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,1 +1,21 @@
 package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func HashPassword(password string) string {}
+
+func VerifyPassword(usersPassword string, givenPassword string) (bool, string) {}
+
+func SignUp() gin.HandlerFunc {}
+
+func Login() gin.HandlerFunc {}
+
+func ProductViewAdmin() gin.HandlerFunc {}
+
+func SearchProduct() gin.HandlerFunc {}
+
+func SearchProductByQuery() gin.HandlerFunc {}
+
+func InstantBuy() gin.HandlerFunc {}
