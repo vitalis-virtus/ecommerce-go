d56e5e04aa36ac8d31e3f3f7ca11a2a0a1b4e391
vitalis-virtus <vshayuk@gmail.com>
2022-12-21 20:00:59 +0200

    add DBSet for creating new Client

--- db/dbSetup.go
+++ db/dbSetup.go
@@ -1,1 +1,41 @@
 package db
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
+
+var Client *mongo.Client = DBSet()
+
+func DBSet() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Println("failed to connect to mongodb")
+		return nil
+	}
+
+	log.Println("Successfully connected to mongodb")
+
+	return client
+}
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {}
+
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection
