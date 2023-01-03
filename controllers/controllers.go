66017dacafd7cc6d51d859455f4e7d956e24f7bb
vitalis-virtus <vshayuk@gmail.com>
2023-01-03 17:49:59 +0200

    implemented signUp and login functions in controllers

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,6 +1,16 @@
 package controllers
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/vitalis-virtus/ecommerce-go/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,9 +18,111 @@ func HashPassword(password string) string {}
 
 func VerifyPassword(usersPassword string, givenPassword string) (bool, string) {}
 
-func SignUp() gin.HandlerFunc {}
+func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
-func Login() gin.HandlerFunc {}
+		var user models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationError := Validate.Struct(user)
+		if validationError != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationError})
+			return
+		}
+
+		count, err := UserCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "user already exists"})
+		}
+
+		count, err = UserCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "this phone number is already in use"})
+			return
+		}
+
+		password := HashPassword(*user.Password)
+		user.Password = &password
+
+		user.Created_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.Updated_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.NewObjectID()
+		user.User_ID = user.ID.Hex()
+
+		token, refreshToken, _ := generate.TokenGenerator(*user.Email, *user.First_Name, *user.Last_Name, user.User_ID)
+		user.Token = &token
+		user.Refresh_Token = &refreshToken
+
+		user.User_Cart = make([]models.ProductUser, 0)
+		user.Address_Details = make([]models.Address, 0)
+		user.Order_Status = make([]models.Order, 0)
+
+		_, insertErr := UserCollection.InsertOne(ctx, user)
+		if insertErr != nil {
+
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "user did not get created"})
+			return
+		}
+
+		c.JSON(http.StatusCreated, "Successfully signed in!")
+	}
+}
+
+func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var user models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		err := UserCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&foundUser)
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "login or password incorrect"})
+			return
+		}
+		PasswordIsvalid, msg := VerifyPassword(*user.Password, *foundUser.Password)
+
+		defer cancel()
+
+		if !PasswordIsvalid {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			fmt.Println(msg)
+			return
+		}
+
+		token, refreshToken, _ := generate.TokenGenerator(*foundUser.Email, *foundUser.First_Name, *foundUser.Last_Name, *foundUser.User_ID)
+		defer cancel()
+
+		generate.UpdateAllTokens(token, refreshToken, foundUser.User_ID)
+
+		c.JSON(http.StatusFound, foundUser)
+	}
+}
 
 func ProductViewAdmin() gin.HandlerFunc {}
 
--- db/dbSetup.go
+++ db/dbSetup.go
@@ -36,6 +36,12 @@ func DBSet() *mongo.Client {
 	return client
 }
 
-func UserData(client *mongo.Client, collectionName string) *mongo.Collection {}
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+	return collection
+}
 
-func ProductData(client *mongo.Client, collectionName string) *mongo.Collection
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
+	var productCollection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+	return productCollection
+}
