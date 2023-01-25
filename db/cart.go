18ed117d64031781b7de327e42ce1a697a8fab95
vitalis-virtus <vshayuk@gmail.com>
2023-01-25 18:18:29 +0200

    implemented AddProductToCart

--- db/cart.go
+++ db/cart.go
@@ -1,7 +1,14 @@
 package db
 
 import (
+	"context"
 	"errors"
+	"log"
+
+	"github.com/vitalis-virtus/ecommerce-go/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var (
@@ -14,7 +21,35 @@ var (
 	ErrCantBuyCartItem    = errors.New("can't update the purchase")
 )
 
-func AddProductToCart() {}
+func AddProductToCart(ctx context.Context, prodCollection, userCollection *mongo.Collection, productID primitive.ObjectID, userID string) error {
+	searchFromdb, err := prodCollection.Find(ctx, bson.M{"_id": productID})
+	if err != nil {
+		log.Println(err)
+		return ErrCantFindProduct
+	}
+
+	var productCart []models.ProductUser
+	err = searchFromdb.All(ctx, &productCart)
+	if err != nil {
+		log.Println(err)
+		return ErrCantDecodeProducts
+	}
+
+	id, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		log.Println(err)
+		return ErrUserIdIsInvalid
+	}
+
+	filter := bson.D{primitive.E{Key: "_id", Value: id}}
+	update := bson.D{{Key: "$push", Value: bson.D{primitive.E{Key: "usercart", Value: bson.D{{Key: "$each", Value: productCart}}}}}}
+
+	_, err = userCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		log.Println(err)
+		return ErrCantUpdateUser
+	}
+}
 
 func RemoveItemFromCart() {}
 
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/gin-gonic/gin v1.8.1
 	github.com/go-playground/validator/v10 v10.10.0
+	github.com/golang-jwt/jwt/v4 v4.4.3
 	github.com/sirupsen/logrus v1.9.0
 	go.mongodb.org/mongo-driver v1.11.1
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d
@@ -15,7 +16,6 @@ require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
-	github.com/golang-jwt/jwt/v4 v4.4.3 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
